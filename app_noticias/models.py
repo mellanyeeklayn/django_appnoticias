e3a2df174e10ea05758f0f3891d1861dc452e00c
mellanyee klayn <47692595+mellanyeeklayn@users.noreply.github.com>
2022-10-20 10:29:43 -0300

    fiat - create new models and views

--- app_noticias/models.py
+++ app_noticias/models.py
@@ -1,7 +1,16 @@
+from tabnanny import verbose
 from django.db import models
 
 class Noticia(models.Model):
-    conteudo = models.TextField()
-    
+    class Meta:
+        verbose_name = 'Notícia'
+        verbose_name_plural = 'Notícias'
+
+    titulo = models.CharField('título', max_length=128)
+        
+    conteudo = models.TextField('conteúdo')
+
+    def __str__(self):
+        return self.titulo    
 
 # Create your models here.
--- app_noticias/views.py
+++ app_noticias/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Noticia
+
+class HomePageView(ListView):
+    model = Noticia
+    template_name = 'app_noticias/home.html'
 
 # Create your views here.
