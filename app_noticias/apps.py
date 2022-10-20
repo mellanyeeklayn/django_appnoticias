509d48d10c6bc75ae7d05b688cc941cebbd652f3
mellanyee klayn <47692595+mellanyeeklayn@users.noreply.github.com>
2022-10-20 09:43:00 -0300

    feat - project_initial

---
+++ app_noticias/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppNoticiasConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app_noticias"
---
+++ app_noticias/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Noticia(models.Model):
+    conteudo = models.TextField()
+    
+
+# Create your models here.
